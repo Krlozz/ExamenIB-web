b879f9280d969d9f2117e55b5f8ee9e85e5ccf1d
Krlozz Ayala <k_arlos210@hotmail.com>
2018-06-02 17:43:13 -0500

    create class

---
+++ project/src/autor/autor.class.ts
@@ -0,0 +1,7 @@
+export class AutorClass {
+    constructor(public nombres:string,
+                public apellidos:string,
+                public fechaNacimiento:string,
+                public numeroLibros:number,
+                public ecuatoriano:boolean){}
+}
---
+++ project/src/libro/libro.class.ts
@@ -0,0 +1,9 @@
+export class LibroClass {
+    constructor(public ICBN:number,
+                public nombre:string,
+                public numeroPaginas:number,
+                public edicion:number,
+                public fechaPublicacion:string,
+                public nombreEditorial:string,
+                public autorID:string){}
+}
